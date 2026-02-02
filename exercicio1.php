97ce0cb2601c7762c8ec22a73bd725b97bbbcb83
Juan Mendes <mendesj0308@gmail.com>
2026-02-02 15:31:21 -0300

    Repositório

---
+++ exercicio1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
+        <?php echo 'Utilizando uma tag padrão';?>
+        <br>
+        <?= 'Utilizando a tag de impressão';?>
+        <br>
+        <?php
+            $a="Meu primeiro arquivo com php"; 
+            echo $a; 
+        ?>
+</body>
+</html>
