79d523326655f7195997cd6e9099eb2a153ea03d
Juan Mendes <mendesj0308@gmail.com>
2026-03-02 17:32:09 -0300

    Envio de repositório

--- HelpDesk_txt/abrir_chamado.php
+++ HelpDesk_txt/abrir_chamado.php
@@ -39,15 +39,15 @@ require_once ('validador_acesso.php');
               <div class="row">
                 <div class="col">
                   
-                  <form>
+                  <form action="registra_chamado.php" method="POST">
                     <div class="form-group">
                       <label>Título</label>
-                      <input type="text" class="form-control" placeholder="Título">
+                      <input name="titulo" type="text" class="form-control" placeholder="Título">
                     </div>
                     
                     <div class="form-group">
                       <label>Categoria</label>
-                      <select class="form-control">
+                      <select class="form-control" name="categoria">
                         <option>Criação Usuário</option>
                         <option>Impressora</option>
                         <option>Hardware</option>
@@ -58,7 +58,7 @@ require_once ('validador_acesso.php');
                     
                     <div class="form-group">
                       <label>Descrição</label>
-                      <textarea class="form-control" rows="3"></textarea>
+                      <textarea name="descricao" class="form-control" rows="3"></textarea>
                     </div>
 
                     <div class="row mt-5">
