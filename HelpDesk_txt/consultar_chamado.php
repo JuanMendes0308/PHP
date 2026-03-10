e7d4097b8184829a44d0f9496ab6a95e66417021
Juan Mendes <mendesj0308@gmail.com>
2026-03-10 16:02:06 -0300

    Envio de repositório

--- HelpDesk_txt/consultar_chamado.php
+++ HelpDesk_txt/consultar_chamado.php
@@ -1,72 +1,132 @@
 <?php
-require_once ('validador_acesso.php');
+
+require_once "validador_acesso.php";
+
+//ARRAY DE CHAMADOS
+$chamados = [];
+
+//CAMINHO DO ARQUIVO
+$caminho = '../../../help_desk_login/registros.txt';
+
+$arquivo = fopen($caminho , 'r');
+
+//LER O ARQUIVO SE VAI ENTRAR OU NAO
+while(!feof($arquivo)){
+
+$registro = fgets($arquivo);
+//PEGA O REGISTRO DE UM ARRAY VAI TER OS CHAMADOS
+$chamados[] = $registro;
+}
+
+fclose($arquivo);  //FECHA
+
 ?>
 
 <html>
-  <head>
-    <meta charset="utf-8" />
-    <title>App Help Desk</title>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<head>
+<meta charset="utf-8" />
+<title>App Help Desk</title>
 
-    <style>
-      .card-consultar-chamado {
-        padding: 30px 0 0 0;
-        width: 100%;
-        margin: 0 auto;
-      }
-    </style>
-  </head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 
-  <body>
+<style>
 
-    <nav class="navbar navbar-dark bg-dark">
-      <a class="navbar-brand" href="home.php">
-        <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
-        App Help Desk
-      </a>
-    </nav>
+.card-consultar-chamado{
+padding:30px 0 0 0;
+width:100%;
+margin:0 auto;
+}
 
-    <div class="container">    
-      <div class="row">
+</style>
 
-        <div class="card-consultar-chamado">
-          <div class="card">
-            <div class="card-header">
-              Consulta de chamado
-            </div>
-            
-            <div class="card-body">
-              
-              <div class="card mb-3 bg-light">
-                <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
+</head>
 
-                </div>
-              </div>
+<body>
 
-              <div class="card mb-3 bg-light">
-                <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
+<nav class="navbar navbar-dark bg-dark">
+<a class="navbar-brand" href="#">
+<img src="logo.png" width="30" height="30">
+App Help Desk
+</a>
+</nav>
 
-                </div>
-              </div>
+<div class="container">    
+<div class="row">
 
-              <div class="row mt-5">
-                <div class="col-6">
-                  <a href="home.php">
-                  <button class="btn btn-lg btn-warning btn-block" type="submit">Voltar</button>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </body>
+<div class="card-consultar-chamado">
+<div class="card">
+
+<div class="card-header">
+Consulta de chamado
+</div>
+
+<div class="card-body">
+
+<?php
+foreach($chamados as $chamado){
+
+$chamado_dados = explode('|', $chamado);
+
+//VERIFICA SE TEM 4 CAMPOS
+if(count($chamado_dados) < 4){
+continue;
+}
+
+//FILTRO DE USUÁRIO
+
+//SE FOR USUÁRIO COMUM
+if(isset($_SESSION['perfil']) && $_SESSION['perfil'] == 'user'){  //SE FOR ADM (VE TUDO) USER (VE SOMENTE O DELE)
+
+    if($_SESSION['id'] != $chamado_dados[0]){
+        continue;
+    }
+
+}
+?>
+
+<div class="card mb-3 bg-light">
+
+<div class="card-body">
+
+<h5 class="card-title">
+<?= $chamado_dados[1] ?>
+</h5>
+
+<h6 class="card-subtitle mb-2 text-muted">
+<?= $chamado_dados[2] ?>
+</h6>
+
+<p class="card-text">
+<?=($chamado_dados[3]) 
+
+?>
+</p>
+
+</div>
+
+</div>
+
+<?php } ?> <!--TERMINA AQUI-->
+
+<div class="row mt-5">
+<div class="col-6">
+
+<a href="home.php" class="btn btn-lg btn-warning btn-block">
+Voltar
+</a>
+
+</div>
+</div>
+
+</div>
+</div>
+</div>
+
+</div>
+</div>
+</div>
+
+</body>
+
 </html>
